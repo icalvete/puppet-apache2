2fee5d764da8b9f094dfceb530e1bdf829375e2c
Israel Calvete <icalvete@gmail.com>
2018-12-04 17:12:39 +0100

    Complaint with ubuntu 18.04.

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,7 +4,24 @@ class apache2::install {
     ensure => present
   }
 
-  package {'libapache2-mod-fastcgi':
-    ensure => present
+  case $facts['os']['distro']['codename'] {
+    'bionic': {
+      file {'get_libapache2-mod-fastcgi':
+        ensure => present,
+        path   => '/tmp/libapache2-mod-fastcgi_2.4.deb',
+        source => "puppet:///modules/${module_name}/libapache2-mod-fastcgi_2.4.deb",
+      }
+      exec {'install_libapache2-mod-fastcgi':
+        command  => '/usr/bin/dpkg -i /tmp/libapache2-mod-fastcgi_2.4.deb; /usr/bin/apt-get -f install',
+        provider => 'shell',
+        unless   => '/usr/bin/dpkg -l "libapache2-mod-fastcgi" | /bin/grep grep ii',
+        require  => File['get_libapache2-mod-fastcgi']
+      }
+    }
+    default: {
+      package {'libapache2-mod-fastcgi':
+        ensure => present
+      }
+    }
   }
 }
